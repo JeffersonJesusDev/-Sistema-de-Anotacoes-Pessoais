f2307099879aa2e8f047210c4ae63fc1d12d9fb7
JeffersonJesusDev <jefferson3680@hotmail.com>
2025-01-10 10:26:21 -0300

    CRUD Service implementado

---
+++ src/main/java/com/jjdev/Sistema/de/Anotacoes/Pessoais/services/NoteService.java
@@ -0,0 +1,40 @@
+package com.jjdev.Sistema.de.Anotacoes.Pessoais.services;
+
+import com.jjdev.Sistema.de.Anotacoes.Pessoais.entities.Note;
+import com.jjdev.Sistema.de.Anotacoes.Pessoais.repositories.NotesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NoteService {
+
+    @Autowired
+    private NotesRepository data;
+
+    public List<Note> getAll() {
+        return data.findAll();
+    }
+
+    public Note saveNote(Note note) {
+        return data.save(note);
+    }
+
+    public Note getNoteById(Long id) {
+        Optional<Note> note = data.findById(id);
+        return note.orElse(null);
+    }
+
+    public void deleteNoteById(Long id) {
+        data.deleteById(id);
+    }
+
+    public Note updateNote(Long id, Note obj) {
+        Note note = getNoteById(id);
+        updateNote(id, obj);
+        return data.save(note);
+    }
+
+}
---
+++ src/main/java/com/jjdev/Sistema/de/Anotacoes/Pessoais/services/UserService.java
@@ -0,0 +1,39 @@
+package com.jjdev.Sistema.de.Anotacoes.Pessoais.services;
+
+import com.jjdev.Sistema.de.Anotacoes.Pessoais.entities.User;
+import com.jjdev.Sistema.de.Anotacoes.Pessoais.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userData;
+
+    public User save(User user) {
+        return userData.save(user);
+    }
+
+    public List<User> findAll() {
+        return userData.findAll();
+    }
+
+    public User findById(Long id) {
+       Optional<User> user = userData.findById(id);
+       return user.orElse(null);
+    }
+
+    public void deleteUser(Long id) {
+        userData.deleteById(id);
+    }
+
+    public User update(Long id, User obj) {
+        User user = findById(id);
+        update(id, obj);
+        return userData.save(user);
+    }
+}
