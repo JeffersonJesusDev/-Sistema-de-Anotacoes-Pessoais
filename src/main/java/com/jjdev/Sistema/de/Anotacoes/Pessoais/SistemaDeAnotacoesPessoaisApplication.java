c5db5de9c59d833ac33ba46b174d0b77e046e59b
JeffersonJesusDev <jefferson3680@hotmail.com>
2025-01-10 09:46:35 -0300

    Commit inicial

---
+++ src/main/java/com/jjdev/Sistema/de/Anotacoes/Pessoais/SistemaDeAnotacoesPessoaisApplication.java
@@ -0,0 +1,13 @@
+package com.jjdev.Sistema.de.Anotacoes.Pessoais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAnotacoesPessoaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAnotacoesPessoaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jjdev/Sistema/de/Anotacoes/Pessoais/SistemaDeAnotacoesPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjdev.Sistema.de.Anotacoes.Pessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeAnotacoesPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
